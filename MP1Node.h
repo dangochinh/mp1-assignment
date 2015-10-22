7d02c799240a2debe906a7710c7014af0498c0b7
Dangngocchinh <dangochinh@gmail.com>
2015-10-22 20:17:47 +0700

    test code 3 Mp1Node.h

--- MP1Node.h
+++ MP1Node.h
@@ -29,10 +29,13 @@
  * Message Types
  */
 enum MsgTypes{
-		JOINREQ,
-		JOINREP,
-		PING,
-		DUMMYLASTMSGTYPE
+    JOINREQ,
+    JOINREP,
+    PING,
+   	HEARTBEATREQ,
+    HEARTBEATREP,
+    DUMMYLASTMSGTYPE
+    
 };
 
 /**
@@ -41,7 +44,7 @@ enum MsgTypes{
  * DESCRIPTION: Header and content of a message
  */
 typedef struct MessageHdr {
-		enum MsgTypes msgType;
+	enum MsgTypes msgType;
 }MessageHdr;
 
 /**
@@ -50,39 +53,50 @@ typedef struct MessageHdr {
  * DESCRIPTION: Class implementing Membership protocol functionalities for failure detection
  */
 class MP1Node {
-		private:
-				EmulNet *emulNet;
-				Log *log;
-				Params *par;
-				Member *memberNode;
-				char NULLADDR[6];
+private:
+	EmulNet *emulNet;
+	Log *log;
+	Params *par;
+	Member *memberNode;
+	char NULLADDR[6];
 
-				void onHeartbeat(Address*, void*, size_t);
-				void onJoin(Address*, void*, size_t);
+public:
+	MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
+	Member * getMemberNode() {
+		return memberNode;
+	}
+	int recvLoop();
+	static int enqueueWrapper(void *env, char *buff, int size);
+	void nodeStart(char *servaddrstr, short serverport);
+	int initThisNode(Address *joinaddr);
+	int introduceSelfToGroup(Address *joinAddress);
+	int finishUpThisNode();
+	void nodeLoop();
+	void checkMessages();
+	bool recvCallBack(void *env, char *data, int size);
+	void nodeLoopOps();
+	int isNullAddress(Address *addr);
+	Address getJoinAddress();
+	void initMemberListTable(Member *memberNode);
+	void printAddress(Address *addr);
+	virtual ~MP1Node();
+	void updateMember(int id, short port, long heartbeat);
+	void updateMember(MemberListEntry& member);
+	bool recvJoinReq(void *env, char *data, int size);
+	bool recvJoinRep(void *env, char *data, int size);
 
-		public:
-				MP1Node(Member *, Params *, EmulNet *, Log *, Address *);
-				Member * getMemberNode() {
-						return memberNode;
-				}
-				int recvLoop();
-				static int enqueueWrapper(void *env, char *buff, int size);
-				void nodeStart(char *servaddrstr, short serverport);
-				int initThisNode(Address *joinaddr);
-				int introduceSelfToGroup(Address *joinAddress);
-				int finishUpThisNode();
-				void nodeLoop();
-				void checkMessages();
-				bool recvCallBack(void *env, char *data, int size);
-				void nodeLoopOps();
-				int isNullAddress(Address *addr);
-				Address getJoinAddress();
-				void initMemberListTable(Member *memberNode, int, short);
-				void printAddress(Address *addr);
-				virtual ~MP1Node();
-				void LogMemberList();
-				void SendHBSomewhere(Address*, long);
-				bool UpdateMemberList(Address*, long);
+	bool recvHeartbeatReq(void *env, char *data, int size);
+	bool recvHeartbeatRep(void *env, char *data, int size);
+
+	bool recvMemberList(const char * label, void *env, char *data, int size);
+
+	void sendMemberList(const char * label, enum MsgTypes msgType, Address *to);
+
+	int memcpyMemberListEntry(char * data, MemberListEntry& member);
+	void logMemberListEntry(MemberListEntry& member);
+
+
+	
 };
 
 #endif /* _MP1NODE_H_ */
